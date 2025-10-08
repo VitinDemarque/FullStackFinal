03c76ccadaf0c38a6c43df31bc7eb35acfd4eb47
Victor Vieira Demarque <victordeemarque7@gmail.com>
2025-10-08 14:15:57 -0300

    Correcao de alguns codigos

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -7,8 +7,10 @@ const router = Router();
  * Auth
  * - Signup
  * - Login
+ * - Refresh (se desejar)
  */
 router.post('/signup', AuthController.signup);
 router.post('/login', AuthController.login);
+router.post('/refresh', AuthController.refreshToken); // opcional
 
 export default router;
