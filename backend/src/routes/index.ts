3c92aa9f4d5d99c49ef1253a84b3307a1aca17c4
Victor Vieira Demarque <victordeemarque7@gmail.com>
2025-10-08 14:23:24 -0300

    correçoes de codigo

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,7 +2,14 @@ import { Router } from 'express';
 
 import authRoutes from './auth.routes';
 import usersRoutes from './users.routes';
-
+import exercisesRoutes from './exercises.routes';
+import submissionsRoutes from './submissions.routes';
+import leaderboardsRoutes from './leaderboards.routes';
+import groupsRoutes from './groups.routes';
+import languagesRoutes from './languages.routes';
+import seasonsRoutes from './seasons.routes';
+import collegesRoutes from './colleges.routes';
+import statsRoutes from './stats.routes';
 
 const router = Router();
 
@@ -11,6 +18,13 @@ const router = Router();
  */
 router.use('/auth', authRoutes);
 router.use('/users', usersRoutes);
-
+router.use('/exercises', exercisesRoutes);
+router.use('/submissions', submissionsRoutes);
+router.use('/leaderboards', leaderboardsRoutes);
+router.use('/groups', groupsRoutes);
+router.use('/languages', languagesRoutes);
+router.use('/seasons', seasonsRoutes);
+router.use('/colleges', collegesRoutes);
+router.use('/stats', statsRoutes);
 
 export default router;
