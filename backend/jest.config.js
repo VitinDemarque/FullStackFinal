483bb5fe90f4fbe278128176f636113b4ff4ca0d
Joao antonio <joaoif.eletro@gmail.com>
2025-10-10 15:57:42 -0300

    Atualizando arquivo de configuracoes do jest no backend

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,11 +1,35 @@
 const { createDefaultPreset } = require("ts-jest");
+//Esta é uma funcao utilitaria do ts-jest, fazendo com que ele entenda TypeScript
 
 const tsJestTransformCfg = createDefaultPreset().transform;
+// Faz a transformação do codigo em TypeScrip para JavaScript permitindo a execução
 
 /** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
+
+  roots: ["<rootDir>/tests"],
+  // Onde o Jest vai procurar os testes
+
+  moduleFileExtensions: ["ts", "js", "json"],
+  // Extensões que o Jest deve entender
+
+  // Transforma arquivos TypeScript
   transform: {
     ...tsJestTransformCfg,
   },
+
+  // Facilita imports absolutos (ex: "@/services/userService")
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+
+  // Ignora pastas desnecessárias nos testes
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+
+  // Gera relatório de cobertura
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
+  coverageDirectory: "coverage",
 };
