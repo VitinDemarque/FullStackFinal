dbd624486c9f5cad84cc5688c254bce2084c6385
Joao antonio <joaoif.eletro@gmail.com>
2025-10-31 12:30:51 -0300

    Atualizando importacao @/types/index no FrontEnd\src\services\api.ts

--- FrontEnd/src/services/api.ts
+++ FrontEnd/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError, AxiosRequestConfig } from 'axios'
-import type { ApiError } from '@types/index'
+import type { ApiError } from '@/types/index'
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
 
